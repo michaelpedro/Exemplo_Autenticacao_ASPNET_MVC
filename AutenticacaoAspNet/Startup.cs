ab8554c39300b459b5fda6ad2579993a653ca313
Michael Pedro - PC <michaelpedroviveiros@outlook.com>
2019-10-01 15:07:26 -0300

    *Foi adicionado a classe [Startup.cs] que será a classe de inicialização requerida pelo OWIN.
    A classe em questão será responsável por dizer que a nossa aplicação está utilizando este mecanismo de autenticação, e qual será a página de login que deverá ser utilizada quando o usuário desejar acessar a parte restrita da aplicação.

---
+++ AutenticacaoAspNet/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(AutenticacaoAspNet.Startup))]
+
+namespace AutenticacaoAspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Define as opções de configuração da autenticação via cookie na aplicação.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                //Tipo de valor padrão do atributo [AuthenticationType].
+                AuthenticationType = "ApplicationCookie",
+
+                /*
+                 * View de login da aplicação.
+                 * Caso o usuário tente acessar alguma view restrita sem estar logado, a biblioteca [OWIN] irá automaticamente
+                 * redirecionar o usuário para a rota da string abaixo.
+                 */
+                LoginPath = new PathString("/Autenticacao/Login")
+            });
+        }
+    }
+}
