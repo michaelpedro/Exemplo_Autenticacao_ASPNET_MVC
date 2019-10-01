709bdc0959cce80d8ece5bc2200122d20ad4c36e
Michael Pedro - PC <michaelpedroviveiros@outlook.com>
2019-10-01 15:49:13 -0300

    *Foi criado uma classe [UsuariosContext] ao projeto para ser o espelho de criação da tabela do banco de dados.
    *Foi configurada uma string de conexao [Conexao] no web.config da aplicação para realizar a conexao ao banco de dados que armazenará os dados dos usuários da aplicação.

---
+++ AutenticacaoAspNet/Models/UsuariosContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutenticacaoAspNet.Models
+{
+    public class UsuariosContext : DbContext
+    {
+        public UsuariosContext() : base("Conexao")
+        {
+
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
