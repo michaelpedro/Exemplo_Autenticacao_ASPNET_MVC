4e2f2fe8a30288182de4502df21688c8a1550fc8
Michael Pedro - PC <michaelpedroviveiros@outlook.com>
2019-10-01 16:01:26 -0300

    *Foi implementada uma model [CadastroUsuarioViewModel que conterá os atributos pertencentes ao cadastro de usuários.

---
+++ AutenticacaoAspNet/Models/CadastroUsuarioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AutenticacaoAspNet.Models
+{
+    public class CadastroUsuarioViewModel
+    {
+        [Required(ErrorMessage = "Informe seu nome")]
+        [MaxLength(100, ErrorMessage = "O nome deve possuir até 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe seu login")]
+        [MaxLength(50, ErrorMessage = "O login deve possuir até 50 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "A senha deve possuir no mínimo 6 caracteres")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Confirmar Senha")]
+        [Required(ErrorMessage = "Informe sua senha")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "A senha deve possuir no mínimo 6 caracteres")]
+        [Compare(nameof(Senha), ErrorMessage = "As senhas não se coincidem")]
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
